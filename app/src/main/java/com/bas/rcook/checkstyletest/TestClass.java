b7264e1d6bfaae1131aad56bfb943bcf934485cb
robcook124 <robcook124@gmail.com>
2015-10-21 16:13:32 -0400

    Uncommenting

--- app/src/main/java/com/bas/rcook/checkstyletest/TestClass.java
+++ app/src/main/java/com/bas/rcook/checkstyletest/TestClass.java
@@ -5,7 +5,7 @@ package com.bas.rcook.checkstyletest;
  *
  *  Uncomment line below to throw an error for the checkstyle task to find
  */
-//import android.app.ActionBar;
+import android.app.ActionBar;
 
 public class TestClass {
 
